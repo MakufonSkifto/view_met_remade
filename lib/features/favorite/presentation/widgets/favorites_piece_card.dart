43fed17be95928781057702693ee76cf5e2aa3f9
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2022-02-11 16:07:56 +0300

    create favorites piece card

---
+++ lib/features/favorite/presentation/widgets/favorites_piece_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../../piece/data/models/piece_model.dart';
+import '../../../piece/presentation/widgets/piece_display_widgets/image_widget.dart';
+
+class FavoritesPieceCard extends StatelessWidget {
+  final PieceModel piece;
+
+  const FavoritesPieceCard({Key? key, required this.piece}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          ListTile(
+            leading: PieceImageWidget(
+              image: piece.primaryImageSmall,
+            ),
+            title: Text(piece.title),
+            subtitle: Text(
+              "by ${piece.artistDisplayName}",
+            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => DetailsPage(id: data["objectID"].toString())),
+                  // );
+                },
+                child: const Text("Details", style: TextStyle(color: Colors.red)),
+              ),
+              TextButton(
+                onPressed: () {
+
+                },
+                child: const Text("Remove from Favorites", style: TextStyle(color: Colors.red)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
