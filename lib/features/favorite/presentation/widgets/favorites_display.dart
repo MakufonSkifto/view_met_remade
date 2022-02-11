a3cbe76e6831b5b2641e47f8bf9f03714e5121fd
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2022-02-11 16:07:48 +0300

    create favorites page

--- lib/features/favorite/presentation/widgets/favorites_display.dart
+++ lib/features/favorite/presentation/widgets/favorites_display.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:view_met_remade/features/favorite/presentation/widgets/favorites_piece_card.dart';
+import 'package:view_met_remade/features/piece/data/models/piece_model.dart';
 
 class FavoritesDisplay extends StatelessWidget {
   final List<String> favorites;
@@ -7,6 +11,13 @@ class FavoritesDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ListView.builder(
+      itemCount: favorites.length,
+      itemBuilder: (BuildContext context, int index) {
+        Map<String, dynamic> data = json.decode(favorites[index]);
+
+        return FavoritesPieceCard(piece: PieceModel.fromJson(data));
+      },
+    );
   }
 }
