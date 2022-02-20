4126e43755ab9715bfe27062da36dc69918990ea
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2022-02-20 15:28:02 +0300

    remove unnecessary imports

--- lib/core/usecases/departments_usecase.dart
+++ lib/core/usecases/departments_usecase.dart
@@ -2,7 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../features/departments/domain/entities/department.dart';
-import '../../features/piece/domain/entities/piece.dart';
 import '../exceptions/failures.dart';
 
 abstract class DepartmentsUseCase<Type, Params> {
