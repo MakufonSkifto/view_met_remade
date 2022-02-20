3ef31ea7e4b52ef32f772c7f41b2802e9137cfcd
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2022-02-20 19:09:37 +0300

    add department container init func and missing bloc providers

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:view_met_remade/features/piece/presentation/pages/piece_page.dart';
 import 'package:view_met_remade/view_met.dart';
+import 'features/departments/presentation/bloc/department_bloc.dart';
+import 'features/favorite/presentation/bloc/favorites_bloc.dart';
 import 'features/piece/presentation/bloc/piece_bloc.dart';
+
 import 'core/injectors/injection_container_piece.dart' as pc; // Piece Injection Container
 import 'core/injectors/injection_container_general.dart' as gc; // General Injection Container
 import 'core/injectors/injection_container_favorite.dart' as fc; // Favorites Injection Container
+import 'core/injectors/injection_container_department.dart' as dc; // Departments Injection Container
 
 
 void main() async {
@@ -15,12 +19,19 @@ void main() async {
   await pc.init();
   await gc.init();
   await fc.init();
+  await dc.init();
 
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
         create: (context) => pc.sl<PieceBloc>()
-      )
+      ),
+      BlocProvider(
+        create: (context) => fc.sl<FavoritesBloc>()
+      ),
+      BlocProvider(
+        create: (context) => dc.sl<DepartmentBloc>()
+      ),
     ],
     child: const ViewMet(page: PiecePage(id: 31)),
   ));
