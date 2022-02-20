d5be06bba64d505b9479d680d2332052b6f7e9d2
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2022-02-20 15:27:41 +0300

    implement department usecases

---
+++ lib/core/usecases/departments_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../features/departments/domain/entities/department.dart';
+import '../../features/piece/domain/entities/piece.dart';
+import '../exceptions/failures.dart';
+
+abstract class DepartmentsUseCase<Type, Params> {
+  Future<Either<Failure, List<Department>>> call();
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
---
+++ lib/features/departments/domain/usecases/get_departments.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:view_met_remade/core/exceptions/failures.dart';
+import 'package:view_met_remade/features/departments/domain/entities/department.dart';
+
+import '../../../../core/usecases/departments_usecase.dart';
+import '../repositories/department_repository.dart';
+
+class GetDepartments extends DepartmentsUseCase<List<Department>, NoParams> {
+  final DepartmentRepository repository;
+
+  GetDepartments(this.repository);
+
+  @override
+  Future<Either<Failure, List<Department>>> call() {
+    return repository.getDepartments();
+  }
+}
