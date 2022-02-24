7c57cd3359e13f4186fdbb66808373426cdddfaf
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2022-02-24 18:36:04 +0300

    fix welcome text time

--- lib/core/widgets/welcome_text.dart
+++ lib/core/widgets/welcome_text.dart
@@ -18,17 +18,23 @@ class WelcomeText extends StatelessWidget {
   Widget builder() {
     var now = DateTime.now();
 
-    if (now.hour <= 11 && now.hour >= 5) {
+    if (now.hour.isBetween(5, 11)) {
       return Text("Good Morning", style: GoogleFonts.playfairDisplaySc(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold));
     }
-    else if (now.hour <= 17 && now.hour >= 12) {
+    else if (now.hour.isBetween(12, 17)) {
       return Text("Good Afternoon", style: GoogleFonts.playfairDisplaySc(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold));
     }
-    else if (now.hour <= 21 && now.hour >= 18 && now.hour >= 19 && now.hour >= 5 || now.hour == 0) {
+    else if (now.hour.isBetween(18, 21) && now.hour.isBetween(5, 19) || now.hour == 0) {
       return Text("Good Evening", style: GoogleFonts.playfairDisplaySc(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold));
     }
     else {
       return Text("Hello", style: GoogleFonts.playfairDisplaySc(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold));
     }
+  }
+}
+
+extension Range on num {
+  bool isBetween(num from, num to) {
+    return from <= this && this <= to;
   }
 }
